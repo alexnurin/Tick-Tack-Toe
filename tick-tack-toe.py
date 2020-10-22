cf91de5301745838a8a27f02e4833c66a4e1f4c7
alexnurin <an362@mail.ru>
2020-10-22 21:56:13 +0300

    baseline tick-tack-toe

---
+++ tick-tack-toe.py
@@ -0,0 +1,32 @@
+class Game(object):
+
+    def __init__(self, field='', name0='Player1', name1='Player2', field_size=3,
+                 in_row_to_win=3):
+        if not field:
+            field = [[-1 for col in range(field_size)] for row in range(field_size)]
+        self.field = field
+        self.name0 = name0
+        self.name1 = name1
+        self.size = field_size
+        self.abreast = in_row_to_win
+
+    def player2name(self, player):
+        if player == 0:
+            return self.name0
+        else:
+            return self.name1
+
+    def whose_move(self):
+        return self.player2name(self.field.count(1) > self.field.count(2))
+
+    def finish_check(self):
+        return 0
+
+    def __str__(self):
+        return '\n'.join([' '.join(str(element)) for element in self.field])
+
+
+if __name__ == '__main__':
+    a = Game(field_size=4, name0='Alex', name1='God')
+    print(a)
+    print(a.whose_move())
